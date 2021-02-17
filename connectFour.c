035deccec531094a96bb20e4e58332df2c17adff
Klaudia <56263298+Zireael-K@users.noreply.github.com>
2021-02-17 23:02:54 +0000

    added connectFour.c

---
+++ connectFour.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void printMenu(){
+
+  printf("\n----------------------------");
+  printf("\n Welcome to Connect Four! \n");
+  printf("----------------------------\n\n");
+  printf(" Start a New Game: \n\n");
+  printf("1 - Single Player \n");
+  printf("2 - Multiplayer \n");
+  printf("3 - Play VS Computer \n");
+  printf("\n ...or choose an option below:\n\n");
+  printf("4 - View game history\n");
+  printf("5 - Settings\n");
+  printf("6 - Exit\n");
+  printf("----------------------------\n");
+}
+
+void printSettingsOptions(){
+
+}
+
+void printGameHistoryMenu(){
+
+}
+
+
+
+int main(){
+  printMenu();
+}
---
+++ makefile
@@ -0,0 +1,6 @@
+game:
+	cl connectFour.c
+clear:
+	del *obj
+	del *exe
+	del *asm
